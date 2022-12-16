13589a75c8e062d641d5fb92490563a15393cc79
Sevag Hanssian <shanssian@nvidia.com>
2022-12-16 14:32:59 -0500

    Cleaner builds by passing entire build command

--- generate-manylinux-docker-args.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-WORKDIR="./manylinux/" MANYLINUX_BUILD_FRONTEND="echo" COMMIT_SHA=latest POLICY="manylinux2014" PLATFORM="x86_64" BASEIMAGE_OVERRIDE="nvidia/cuda:11.8.0-devel-centos7" ./manylinux/build.sh
-WORKDIR="./manylinux/" MANYLINUX_BUILD_FRONTEND="echo" COMMIT_SHA=latest POLICY="manylinux2014" PLATFORM="x86_64" BASEIMAGE_OVERRIDE="nvidia/cuda:12.0.0-devel-centos7" ./manylinux/build.sh
-WORKDIR="./manylinux/" MANYLINUX_BUILD_FRONTEND="echo" COMMIT_SHA=latest POLICY="manylinux_2_31" PLATFORM="aarch64" BASEIMAGE_OVERRIDE="nvidia/cuda:11.8.0-devel-ubuntu20.04" ./manylinux/build.sh
-WORKDIR="./manylinux/" MANYLINUX_BUILD_FRONTEND="echo" COMMIT_SHA=latest POLICY="manylinux_2_31" PLATFORM="aarch64" BASEIMAGE_OVERRIDE="nvidia/cuda:12.0.0-devel-ubuntu20.04" ./manylinux/build.sh
