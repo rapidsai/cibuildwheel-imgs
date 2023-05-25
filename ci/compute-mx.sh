fe70ee1511f3954e9506fca6dc7f55015735733d
divyegala <divyegala@gmail.com>
2023-05-25 12:25:20 -0700

    address review comments

--- ci/compute-mx.sh
+++ ci/compute-mx.sh
@@ -3,6 +3,3 @@
 set -euo pipefail
 
 yq -o json axis.yaml | jq -c 'include "ci/compute-mx"; compute_mx(.)'
-
-# MATRIX=$(yq -o json '.' axis.yaml | jq -c)
-# echo "MATRIX=${MATRIX}" | tee --append ${GITHUB_OUTPUT:-/dev/null}
