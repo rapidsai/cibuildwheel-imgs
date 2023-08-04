59ba6ca37ff15050703d44917ebf097234234673
Jake Awe <jawe@nvidia.com>
2023-08-04 09:59:28 -0500

    compute aot

--- ci/compute-mx.sh
+++ ci/compute-mx.sh
@@ -2,4 +2,9 @@
 
 set -euo pipefail
 
-yq -o json axis.yaml | jq -c 'include "ci/compute-mx"; compute_mx(.)'
+export PR_NUM=34 #"${GITHUB_REF_NAME##*/}"
+export BUILD_TYPE="branch"
+
+yq -o json axis.yaml | jq -c 'include "ci/compute-mx"; compute_mx(.)' --arg pr_num "${PR_NUM}" --arg build_type "${BUILD_TYPE}"
+
+
