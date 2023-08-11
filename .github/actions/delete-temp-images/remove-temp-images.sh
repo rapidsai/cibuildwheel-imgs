2c4a813d4db89cd45b7368b22689fbd65dff99e6
Jake Awe <jawe@nvidia.com>
2023-08-11 12:23:39 -0500

    mv actions scripts

---
+++ .github/actions/delete-temp-images/remove-temp-images.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+HUB_TOKEN=$(
+    curl -s -H "Content-Type: application/json" \
+    -X POST \
+    -d "{\"username\": \"${GPUCIBOT_DOCKERHUB_USER}\", \"password\": \"${GPUCIBOT_DOCKERHUB_TOKEN}\"}" \
+    https://hub.docker.com/v2/users/login/ | jq -r .token \
+)
+echo "::add-mask::${HUB_TOKEN}"
+
+org=${IMAGE_NAME%%/*}
+temp=${IMAGE_NAME#*/}
+repo=${temp%%:*}
+tag=${IMAGE_NAME##*:}
+
+for arch in $(echo "$ARCHES" | jq .[] -r); do
+    curl --fail-with-body -i -X DELETE \
+    -H "Accept: application/json" \
+    -H "Authorization: JWT $HUB_TOKEN" \
+    "https://hub.docker.com/v2/repositories/$org/$repo/tags/$tag-$arch/"
+done
