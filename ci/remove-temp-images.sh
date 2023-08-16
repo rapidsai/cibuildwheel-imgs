81510d532ff3863abcb7bf9b5756ee4925da6428
Jake Awe <jawe@nvidia.com>
2023-08-16 12:03:13 -0500

    formatting

--- ci/remove-temp-images.sh
+++ ci/remove-temp-images.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+set -euo pipefail
 
 HUB_TOKEN=$(
-    curl -s -H "Content-Type: application/json" \
+  curl -s -H "Content-Type: application/json" \
     -X POST \
     -d "{\"username\": \"${GPUCIBOT_DOCKERHUB_USER}\", \"password\": \"${GPUCIBOT_DOCKERHUB_TOKEN}\"}" \
     https://hub.docker.com/v2/users/login/ | jq -r .token \
@@ -14,7 +15,7 @@ repo=${temp%%:*}
 tag=${IMAGE_NAME##*:}
 
 for arch in $(echo "$ARCHES" | jq .[] -r); do
-    curl --fail-with-body -i -X DELETE \
+  curl --fail-with-body -i -X DELETE \
     -H "Accept: application/json" \
     -H "Authorization: JWT $HUB_TOKEN" \
     "https://hub.docker.com/v2/repositories/$org/$repo/tags/$tag-$arch/"
@@ -22,5 +23,5 @@ done
 
 # Logout
 curl -X POST \
--H "Authorization: JWT $HUB_TOKEN" \
-"https://hub.docker.com/v2/logout/"
+  -H "Authorization: JWT $HUB_TOKEN" \
+  "https://hub.docker.com/v2/logout/"
