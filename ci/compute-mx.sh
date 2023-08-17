3d24cb752ed17af4e2b6a2a8fe950c372444fa13
Jake Awe <50372925+AyodeAwe@users.noreply.github.com>
2023-08-17 11:40:10 -0500

    Conditionally delete temporary build images (#33)
    See PR description
    
    * fix img deletion
    
    * incorporate multi-arch build & delete for PR
    
    * DRY
    
    * compute aot
    
    * update curl command
    
    * rm pr clean-up
    
    * dedupe checkout
    
    * parametize tags
    
    * formatting
    
    * compute image name in jq
    
    * incorporate image_name
    
    * fix checkout order
    
    * rename jobs
    
    * some refactoring
    
    * rename mx to matrix
    
    * convert jobs to composite actions
    
    * fix param, indent
    
    * dedupe checkouts
    
    * fix
    
    * compute manylinux in jq
    
    * mv actions scripts
    
    * rm composites
    
    * formatting
    
    * small fixes

--- ci/compute-mx.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-PR_NUM=${PR_NUM:-""}
-BUILD_TYPE=${BUILD_TYPE:-""}
-
-yq -o json axis.yaml | jq -c 'include "ci/compute-mx"; compute_mx(.)' --arg pr_num "${PR_NUM}" --arg build_type "${BUILD_TYPE}"
-
